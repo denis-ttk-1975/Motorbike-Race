887482109127ee3952feceee486d8023b1b73125
denis-ttk-1975 <denis.ttk@gmail.com>
2021-06-22 17:47:50 +0300

    requestAnimationFrame created

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
-let 
+const score = document.querySelector('.score'),
+		start = document.querySelector('.start'),
+		gameArea = document.querySelector('.gameArea');
+
+const keys = {
+	ArrowUp: false,
+	ArrowDown: false,
+	ArrowRight: false,
+	ArrowLeft: false,
+};
+
+const setting = {
+	start: false,
+	score: 0,
+	speed: 3,
+}
+
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
+
+function startGame() {
+	start.classList.add('hide');
+	requestAnimationFrame(playGame);
+};
+
+function playGame() {
+	console.log('start game');
+	requestAnimationFrame(playGame);
+
+};
+
+function startRun(event) {
+		event.preventDefault();
+		keys[event.key] = true;
+};
+
+function stopRun(event) {
+		event.preventDefault();
+		keys[event.key] = false;
+};
+
