0e774e9e6cb1dbaab6bb5fee68ec5d10602dad9c
denis-ttk-1975 <denis.ttk@gmail.com>
2021-06-25 16:11:07 +0300

    enemyCars added

--- main.js
+++ main.js
@@ -15,8 +15,9 @@ const keys = {
 const setting = {
 	start: false,
 	score: 0,
-	speed: 3,
-}
+	speed: 5,
+	traffic: 3,
+};
 
 start.addEventListener('click', startGame);
 document.addEventListener('keydown', startRun);
@@ -26,12 +27,23 @@ function startGame() {
 	start.classList.add('hide');
 	setting.start = true;
 
-	for (let i = 0; i < 20; i++) {
+	for (let i = 0; i < getQuantityElements(100); i++) {
 		const line = document.createElement('div');
 		line.classList.add('line');
 		line.style.top = (i * 100) + 'px';
 		line.y = i * 100;
 		gameArea.appendChild(line);
+
+	}
+
+	for (let i = 0; i < getQuantityElements(100 * setting.traffic); i++) {
+		const enemy = document.createElement('div');
+		enemy.classList.add('enemy');
+		enemy.y = -100 * setting.traffic * (i + 1);
+		enemy.style.top = enemy.y + 'px';
+		enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+		enemy.style.backgroundImage = 'url(./image/enemy.png)';
+		gameArea.appendChild(enemy);
 
 	}
 	
@@ -46,6 +58,7 @@ function playGame() {
 	if (setting.start) {
 		
 		moveRoad();
+		moveEnemy();
 
 		if (keys.ArrowLeft && setting.x > 3) {
 			setting.x -= setting.speed;
@@ -79,15 +92,33 @@ function stopRun(event) {
 		keys[event.key] = false;
 };
 
+function getQuantityElements(heightElement) {
+	return Math.ceil(document.documentElement.clientHeight / heightElement + 1);
+}
+
 function moveRoad() {
 	let lines = document.querySelectorAll('.line');
 	lines.forEach(function(line) {
 		line.y += setting.speed;
 		line.style.top = line.y + 'px';
-		if (line.y > document.documentElement.clientHeight) {
+		if (line.y >= document.documentElement.clientHeight) {
 			line.y = -100;
 		}
-	})
-
+	});
 }
 
+function moveEnemy() {
+	let enemy = document.querySelectorAll('.enemy');
+	enemy.forEach(function(item) {
+		item.y += setting.speed / 2;
+		item.style.top = item.y + 'px';
+		if (item.y >= document.documentElement.clientHeight) {
+			item.y = -100 * setting.traffic;
+			item.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + 'px';
+			if (Math.random() * 2 < 1) {
+				item.style.backgroundImage = 'url(./image/enemy2.png)';
+			}
+		}
+
+	});
+}
